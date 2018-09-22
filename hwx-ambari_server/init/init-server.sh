6c6f0b7d728cfb15ab4323f23d24c13f6b92c89d
AliGouta <ali.gouta@gmail.com>
2018-09-23 00:37:50 +0200

    Update init-server.sh

--- hwx-ambari_server/init/init-server.sh
+++ hwx-ambari_server/init/init-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-: ${CLOUD_PLATFORM:="none"}
-: ${USE_CONSUL_DNS:="true"}
+#: ${CLOUD_PLATFORM:="none"}
+#: ${USE_CONSUL_DNS:="true"}
 
 [[ "TRACE" ]] && set -x
 
@@ -9,28 +9,28 @@ debug() {
   [[ "DEBUG" ]]  && echo "[DEBUG] $@" 1>&2
 }
 
-get_nameserver_addr() {
-  if [[ "$NAMESERVER_ADDR" ]]; then
-    echo $NAMESERVER_ADDR
-  else
-    if ip addr show docker0 &> /dev/null; then
-      ip addr show docker0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
-    else
-      ip ro | grep default | cut -d" " -f 3
-    fi
-  fi
-}
+#get_nameserver_addr() {
+#  if [[ "$NAMESERVER_ADDR" ]]; then
+#    echo $NAMESERVER_ADDR
+#  else
+#    if ip addr show docker0 &> /dev/null; then
+#      ip addr show docker0 | grep "inet\b" | awk '{print $2}' | cut -d/ -f1
+#    else
+#      ip ro | grep default | cut -d" " -f 3
+#    fi
+#  fi
+#}
 
 # --dns isn't available for: docker run --net=host
 # sed -i /etc/resolf.conf fails:
 # sed: cannot rename /etc/sedU9oCRy: Device or resource busy
 # here comes the tempfile workaround ...
-local_nameserver() {
-  cat>/etc/resolv.conf<<EOF
-nameserver $(get_nameserver_addr)
-search service.consul node.dc1.consul
-EOF
-}
+#local_nameserver() {
+#  cat>/etc/resolv.conf<<EOF
+#nameserver $(get_nameserver_addr)
+#search service.consul node.dc1.consul
+#EOF
+#}
 
 wait_for_db() {
   while : ; do
@@ -42,11 +42,11 @@ wait_for_db() {
 
 # GCP overrides the /etc/hosts file with its internal hostname, so we need to change the
 # order of the host resolution to try the DNS first
-reorder_dns_lookup() {
-  if [ "$CLOUD_PLATFORM" == "GCP" ] || [ "$CLOUD_PLATFORM" == "GCC" ]; then
-    sed -i "/^hosts:/ s/ *files dns/ dns files/" /etc/nsswitch.conf
-  fi
-}
+#reorder_dns_lookup() {
+#  if [ "$CLOUD_PLATFORM" == "GCP" ] || [ "$CLOUD_PLATFORM" == "GCC" ]; then
+#    sed -i "/^hosts:/ s/ *files dns/ dns files/" /etc/nsswitch.conf
+#  fi
+#}
 
 config_remote_jdbc() {
   if [ -z "$POSTGRES_DB" ]
@@ -80,8 +80,8 @@ silent_security_setup() {
 }
 
 main() {
-  [[ "$USE_CONSUL_DNS" == "true" ]] && local_nameserver
-  reorder_dns_lookup
+  #[[ "$USE_CONSUL_DNS" == "true" ]] && local_nameserver
+  #reorder_dns_lookup
   if [ ! -f "/var/ambari-init-executed" ]; then
     config_remote_jdbc
     silent_security_setup
